8d8ea3fc586ec26b99ce760d8adfa54af1c394fe
Renu <renu.vishwakarma@w3villa.com>
2023-05-03 13:02:35 +0530

    add association

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -22,7 +22,7 @@ class ArticlesController < ApplicationController
   # POST /articles or /articles.json
   def create
     @article = Article.new(article_params)
-
+     @article.user = User.first
     respond_to do |format|
       if @article.save
         format.html { redirect_to article_url(@article), notice: "Article was successfully created." }
@@ -46,6 +46,7 @@ class ArticlesController < ApplicationController
       end
     end
   end
+  validates :title, presence: true, length: { minimum: 6, maximim: 100 }
 
   # DELETE /articles/1 or /articles/1.json
   def destroy
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,6 @@
 class Article < ApplicationRecord
+    belongs_to :user
+    validates :title, presence: true, length: { minimum: 6, maximim: 80 }
+    validates :description, presence: true, length: { minimum: 10, maximim: 200 }
+
 end
